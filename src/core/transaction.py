7e5a3b1bce80b5481f576441396ca751adb86aa1
NeZorinEgor <zorin-ep@mail.ru>
2024-10-27 17:49:07 +0800

    feat: monore

--- src/core/transaction.py
+++ src/core/transaction.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import List
 
+from src.models.storehouse_transaction import StorehouseTransaction
+
 
 class TransactionProcessor(ABC):
     @abstractmethod
-    def preprocess(self, transaction: List):
+    def preprocess(self, transactions: List[StorehouseTransaction]):
         pass
--- src/models/turnhover_calculator.py
+++ src/models/turnhover_calculator.py
@@ -6,22 +6,25 @@ from src.models.storehouse_transaction import TransactionType, StorehouseTransac
 
 
 class TurnoverCalculator(TransactionProcessor):
-    def preprocess(self, transactions: List[StorehouseTransaction]) -> List[StorehouseTurnover]:
-        # Словарь для хранения оборотов по каждому складу
+    def preprocess(self, transactions: List[StorehouseTransaction], start_date: datetime.datetime,
+                end_date: datetime.datetime) -> List[StorehouseTurnover]:
         turnover_data = {}
 
-        # Обрабатываем каждую транзакцию
-        for transaction in transactions:
-            storehouse = transaction.storehouse
+        # Фильтруем транзакции по дате
+        filtered_transactions = [t for t in transactions if start_date <= t.time <= end_date]
+
+        # Рассчитываем обороты
+        for transaction in filtered_transactions:
+            key = (transaction.storehouse, transaction.nomenclature, transaction.range)
             turnover_change = transaction.quantity if transaction.transaction_type == TransactionType.INBOUND else -transaction.quantity
 
-            if storehouse not in turnover_data:
-                turnover_data[storehouse] = StorehouseTurnover.create(
-                    storehouse=storehouse,
+            if key not in turnover_data:
+                turnover_data[key] = StorehouseTurnover(
+                    storehouse=transaction.storehouse,
                     turnover=0,
                     nomenclature=transaction.nomenclature,
                     range=transaction.range
                 )
-            turnover_data[storehouse].turnover += turnover_change
+            turnover_data[key].turnover += turnover_change
 
         return list(turnover_data.values())
